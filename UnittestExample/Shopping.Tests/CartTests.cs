1f8e1973d0e9bc7f76d7c973c916e462a2df4631
Nazir Çevik <nzr.cevik@hotmail.com>
2020-11-25 23:48:43 +0300

    Finsih test

---
+++ UnittestExample/Shopping.Tests/CartTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnittestExample;
+
+namespace Shopping.Tests
+{
+    [TestClass]
+    public class CartTests
+    {
+        [TestMethod]
+        public void Sepete_Urun_Eklenebilir()
+        {
+            //Arange
+            const int beklenen = 1;
+            var cartManager = new CartManager();
+            var cartItem = new CartItem
+            {
+                Product = new Product
+                {
+                    ProductId = 1,
+                    ProductName = "Laptop",
+                    UnitPrice = 2500
+                },
+                Quantity = 1
+            };
+
+            //act
+            cartManager.Add(cartItem);
+            var toplamelamansayisi = cartManager.TotalItems;
+            //Assert
+            Assert.AreEqual(beklenen, toplamelamansayisi);
+
+        }
+
+        [TestMethod]
+        public void Sepette_olan_urun_cikarilabilmelidir()
+        {
+            //Arange
+            var cartManager = new CartManager();
+            var cartItem = new CartItem { 
+            Product = new Product { ProductId=1,ProductName="LAptop",UnitPrice=2500},Quantity=1
+            };
+            cartManager.Add(cartItem);
+            var sepetteolanelamansayisi = cartManager.TotalItems;
+            //act
+            cartManager.Remove(1);
+            var sepettekalanelamansayisi = cartManager.TotalItems;
+            //Assert
+            Assert.AreEqual(sepetteolanelamansayisi - 1, sepettekalanelamansayisi);
+
+        }
+
+
+        [TestMethod]
+        public void Sepet_temizlenebilir()
+        {
+            //Arange
+            var cartManager = new CartManager();
+            var cartItem = new CartItem
+            {
+                Product = new Product { ProductId = 1, ProductName = "LAptop", UnitPrice = 2500 },
+                Quantity = 1
+            };
+            cartManager.Add(cartItem);
+            //act
+            cartManager.Clear();
+            //Assert
+            Assert.AreEqual(0,cartManager.TotalQuantity);
+            Assert.AreEqual(0, cartManager.TotalItems);
+
+        }
+    }
+}
---
+++ UnittestExample/Shopping.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Shopping.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Shopping.Tests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f3bbd022-98c5-4401-bde9-e60fb2b85fb1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
