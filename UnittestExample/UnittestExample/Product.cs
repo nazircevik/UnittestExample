67be96e3aa6cf33a1e21da073cf782a957af3edb
Nazir Çevik <nzr.cevik@hotmail.com>
2020-11-25 22:14:44 +0300

    cartitem

--- UnittestExample/UnittestExample/Product.cs
+++ UnittestExample/UnittestExample/Product.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace UnittestExample
 {
-    class Product
+    public class Product
     {
         public int ProductId { get; set; }
         public  string ProductName { get; set; }
