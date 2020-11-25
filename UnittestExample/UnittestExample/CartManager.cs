b3d4692be79e487554fd99273fc3dd2ca6be80bc
Nazir Çevik <nzr.cevik@hotmail.com>
2020-11-25 22:30:11 +0300

    cartmanager

---
+++ UnittestExample/UnittestExample/CartManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnittestExample
+{
+    class CartManager
+    {
+        private readonly List<CartItem> _cartItems;
+        public CartManager()
+        {
+            _cartItems = new List<CartItem>();
+        }
+        public void Add(CartItem cartItem)
+        {
+            _cartItems.Add(cartItem);
+        }
+        public void Remove(int ProductId)
+        {
+            var product = _cartItems.FirstOrDefault(t => t.Product.ProductId == ProductId);
+            _cartItems.Remove(product);
+        }
+        public List<CartItem> CartItems
+        {
+            get
+            {
+                return _cartItems;
+            }
+        }
+
+        public void Clear()
+        {
+            _cartItems.Clear();
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+
+                return _cartItems.Sum(t => t.Quantity); 
+            }
+        }
+        public int TotalQuantity
+        {
+            get { return _cartItems.Sum(t => t.Quantity); }
+        }
+        public int TotalItems
+        {
+            get { return _cartItems.Count; }
+        }
+        
+
+    }
+}
